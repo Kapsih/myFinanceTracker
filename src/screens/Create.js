e569c8ce0f160786b85ad51bd9bbdc4564261796
Anish Kuber <anish.kuber@gmail.com>
2026-03-21 16:51:14 +0530

    Designed Create expense screen. Add temp values

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,12 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View, ScrollView, TextInput, Pressable, Alert } from 'react-native'
+import { useState } from 'react';
+import tailwind from 'twrnc'
 const Create = () => {
+const [amount,setAmount] = useState(null);
+const [title,setTitle] = useState("");
+
+const handleAddExpense = ()=>
+  {
+    if(!amount || !title){
+      console.log("All fields are required");
+      Alert.alert("All fields are required");
+      return;
+    }
+    console.log("amount", amount);
+    console.log("title", title);
+  }
+
   return (
     <View>
-      <Text>Create</Text>
+      <ScrollView contentContainerStyle={tailwind`p-6`}>
+        {/*Header Section*/}
+        <Text style={tailwind`text-3xl font-bold text-black`}>Add new expense</Text>
+        <Text style={tailwind`text-base text-gray-500 mt-2 mb-8`}>Enter the details of your expense to help you track your expenses</Text>
+        {/*Expense Form Section*/}
+
+        <View style={tailwind`mb-5`}>
+          <Text style={tailwind`text-xl font-semibold text-gray-600 mb-2`}>
+            Enter amount
+          </Text>
+          <TextInput 
+              placeholder="$0.00" 
+              style={tailwind`border-2 border-gray-300 p-4 rounded-xl text-xl`}
+              value = {amount}
+              onChangeText ={setAmount}
+              />
+        </View>
+
+        <View style={tailwind`mb-5`}>
+          <Text style={tailwind`text-xl font-semibold text-gray-600 mb-2`}>
+            Title
+          </Text>
+          <TextInput 
+              placeholder="What was this for?" 
+              style={tailwind`border-2 border-gray-300 p-4 rounded-xl text-xl`}
+              value = {title}
+              onChangeText ={setTitle}
+              />
+        </View>
+        {/*Input 3- category tbd*/}
+        <View style={tailwind`mb-5`}>
+          <Text style={tailwind`text-xl font-semibold text-gray-600 mb-2`}>
+            Category
+          </Text>
+          <Pressable style={tailwind`border border-gray-400 p-4 rounded-xl flex-row justify-between items-center`}>
+            <View style={tailwind`flex-row items-center`}>
+              <Text style={tailwind`text-2xl mr-3`}>🍜</Text>
+              <Text style={tailwind`text-lg`}>{"Food"}</Text>
+            </View>
+            <Text style={tailwind`text-2xl`}>&gt;</Text>
+          </Pressable>
+        </View>
+
+        {/*Footer Section*/}
+        <Pressable style={tailwind`bg-black p-4 rounded-lg mt-8`} onPress={handleAddExpense}>
+          <Text style={tailwind`text-white text-center text-lg font-bold`}>Add Expense</Text>
+        </Pressable>
+      </ScrollView>
     </View>
-  )
+  ) 
 }
 
 export default Create
