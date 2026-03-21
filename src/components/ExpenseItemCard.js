bd88f4b37eb82d8d1f98d276de4df756941757fd
Anish Kuber <anish.kuber@gmail.com>
2026-03-21 12:22:13 +0530

    created and designed ExpenseItemCard with temp data

---
+++ src/components/ExpenseItemCard.js
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, ToastAndroid, View } from 'react-native'
+import React from 'react'
+import tailwind from 'twrnc'
+
+const ExpenseItemCard = ({item}) => {
+  return (
+    <View style={tailwind`bg-white rounded-2xl p-4 mx-5 mb-3 flex-row justify-between items-center shadow-sm`}>
+      <View style={tailwind`flex-row items-center`}>
+            {/*Item Icon*/}
+            <View style={tailwind`w-12 h-12 rounded-xl bg-gray-100 justify-center items-center mr-4`}>
+                <Text>{"☕"}</Text>
+            </View>
+            {/*Item Name*/}
+            <View>
+                <Text style={tailwind`text-base font-bold text-gray-800`}>{"Coffee"}</Text>
+                {/*Item Category*/}
+                <View style={tailwind`mt-1 px-2 py-1 rounded-lg self-start bg-orange-400`}>
+                    <Text style={tailwind`text-xs font-bold text-gray-700`}>{"Food and drinks"}</Text>
+                </View>
+            </View>
+       
+      </View>
+        {/*Item Info- cost and date*/}
+            <View style={tailwind`items-end`}>
+                <Text style={tailwind`text-base font-bold text-black`}>{"100$"}</Text>
+                <Text style={tailwind`text-xs text-gray-500 mt-1`}>{"21-03-2026"}</Text>
+            </View>
+    </View>
+  )
+}
+
+export default ExpenseItemCard
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,33 @@
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native'
+
+import tailwind from 'twrnc'
+import EmptyList from '../components/EmptyList'
+import ExpenseItemCard from '../components/ExpenseItemCard'
+
 
 export default function Home({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
+    <View >
+      <View style={tailwind`px-5 pt-5 pb-3`}>
+        <Text style={tailwind`font-bold text-4xl text-black`}>Hello👋</Text>
+        <Text style={tailwind`text-base text-gray-500 mt-1`}>Start tracking your expenses easily</Text>
+      </View>
 
-      <Button title='Go to Profile'  onPress={() => navigation.navigate("Profile")}/>
+      <View style={tailwind`bg-black rounded-3xl p-6 my-5 mx-5 items-center shadow-lg`}>
+        <Text style={tailwind`text-base text-gray-400`}>Spent so far</Text>
+        <Text style={tailwind`text-base text-white text-4xl font-bold mt-2 `}>200$</Text>
+      </View>
+
+      <FlatList
+        data = {[1,2,3]}
+        renderItem = {({item})=> <ExpenseItemCard item={item}/>}
+        keyExtractor = {(item)=> item}
+        contentContainerStyle = {{paddingBottem : 20}}
+        ListEmptyComponent={<EmptyList/>}
+      >
+
+      </FlatList>
+      {/*<Button title='Go to Profile'  onPress={() => navigation.navigate("Profile")}/>*/}
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
